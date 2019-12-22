d4cade3b52a5424012d52c6ef868be5bdf3ff57b
Ilya Krylov <kiaplayer@gmail.com>
2019-12-22 11:55:14 +0300

    Part3 - css modules

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-full" :class="$style.centered">
+    <h1 :class="$style.headingLarge">Not found</h1>
+    <p :class="$style.textLarge">
+      Oops, we couldn't find what you are looking for. Why don't you
+      <router-link :to="{name:'Home'}">
+        go home instead?
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PageNotFound'
+  }
+</script>
+
+<style module>
+  .headingLarge {
+    font-size: 100px;
+  }
+  .textLarge {
+    font-size: 50px;
+    font-weight: 100;
+  }
+  .centered {
+    text-align: center;
+  }
+</style>
