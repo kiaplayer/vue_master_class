1f2aa5e36f9e4d275ae6ff2690806b9cc6bc7aab
Ilya Krylov <kiaplayer@gmail.com>
2020-01-02 12:03:16 +0300

    Part 4 - Destructuring JavaScript Arrays and Objects

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -39,8 +39,7 @@
       }
     },
     methods: {
-      addNewPost (eventData) {
-        const post = eventData.post
+      addNewPost ({post}) {
         const postId = post['.key']
         this.$set(sourceData.posts, postId, post)
         this.$set(this.thread.posts, postId, postId)
