d6da68d1e3842c0fd598beb211b5d06c7c69bb06
Ilya Krylov <kiaplayer@gmail.com>
2020-01-02 11:35:10 +0300

    Part 4 - add new post

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -4,6 +4,21 @@
     <PostList
       :posts="posts"
     />
+    <form @submit.prevent="addNewPost">
+      <div class="form-group">
+        <textarea
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+          class="form-input"
+          v-model="newPostText"
+        />
+      </div>
+      <div class="form-actions">
+        <input type="submit" class="btn-blue" value="Submit post">
+      </div>
+    </form>
   </div>
 </template>
 
@@ -23,13 +38,30 @@
     },
     data () {
       return {
-        thread: sourceData.threads[this.id]
+        thread: sourceData.threads[this.id],
+        newPostText: ''
       }
     },
     computed: {
       posts () {
         const postIds = Object.values(this.thread.posts)
         return Object.values(sourceData.posts).filter(post => postIds.includes(post['.key']))
+      }
+    },
+    methods: {
+      addNewPost () {
+        const postId = 'greatPost' + Math.random()
+        const post = {
+          text: this.newPostText,
+          userId: 'jUjmgCurRRdzayqbRMO7aTG9X1G2',
+          threadId: this.id,
+          publishedAt: (new Date()).getTime(),
+          '.key': postId
+        }
+        this.$set(sourceData.posts, postId, post)
+        this.$set(this.thread.posts, postId, postId)
+        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.newPostText = ''
       }
     }
   }
